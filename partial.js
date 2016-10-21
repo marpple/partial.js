bc94a66771978eac53048a807ace6d0835fccd4a
hanahdool <choi.hanah92@gmail.com>
2016-10-21 13:41:26 +0900

    partial.js
    - _.pipe(), _.mr() 추가

--- partial.js
+++ partial.js
@@ -97,7 +97,7 @@
     var funcs = _.flatten(arguments);
     var i = 0;
     return function(arg) {
-      //if ( i == funcs.length) return arg;
+      if ( i == funcs.length) return arg;
       //var args = arg._is_mr ? arg : arguments ;
       //return arguments.callee(funcs[i++].apply(null, args));
       return arguments.callee(funcs[i++].apply(null, arg._is_mr ? arg : arguments));
